946597da307946376542c775cf9621d7eb575411
Magdy Ayman <140098717+MagdyAyman6@users.noreply.github.com>
2024-12-02 19:08:23 +0200

    Delete oli_elazm directory

--- oli_elazm/lib/core/utils/assets.dart
+++
@@ -1,6 +0,0 @@
-abstract class AssetsData{
-  static const splash = 'assets/images/splash.jpeg';
-  static const onBoarding1 = 'oli_elazm/assets/images/onboarding_1.jpg';
-  static const onBoarding2 = 'oli_elazm/assets/images/onboarding_2.png';
-  static const onBoarding3 = 'oli_elazm/assets/images/onboarding_3.png';
-}
